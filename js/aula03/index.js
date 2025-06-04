5bb8f2605772f7b1205c2f27ac27e3703f832c03
VitorrAmorim <vi08tor@gmail.com>
2025-06-03 22:32:01 -0300

    continuando exercicios - aula03: Implementando JS e HTML - aula04: Iniciando váriaveis (a continuar)

---
+++ js/aula03/index.js
@@ -0,0 +1,6 @@
+// Um comentário
+// Usar a tag script dentro do HEAD pode retardar o carregamento da página
+// O melhor a se fazer é criar um arquivo próprio para cada usabilidade
+console.log("Olá Mundo!"); // Só será exibido no terminal ou no Console
+console.log("São Paulo é o maior time do Brasil!");
+alert("Palmeiras não tem mundial!");
---
+++ js/aula04/index.js
@@ -0,0 +1,8 @@
+// Váriaveis é algo que pode ser modificado/mudado
+
+// Sempre se utiliza let para criar váriaveis
+let nome = "Vitor"; // String
+
+console.log(nome, "nasceu em 2006");
+console.log("O", nome, "tirou 10 na máteria de Banco de Daods");
+console.log("Eu", nome, "sou um programador");
